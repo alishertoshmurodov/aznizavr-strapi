23b8735f53c7921bb9258eacf680fcae7d809642
Alisher <toshmurodov2001@mail.ru>
2024-10-23 10:00:02 +0500

    adding app.yaml

---
+++ src/api/anime-banner/services/anime-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * anime-banner service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::anime-banner.anime-banner');
